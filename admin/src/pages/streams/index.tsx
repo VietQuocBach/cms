54e97d001cf65fe5b4ddde957d39e7d108a67b9a
VietQuocBach <vietbq@danateq.vn>
2024-11-26 02:05:54 +0700

    wip

--- admin/src/pages/streams/index.tsx
+++ admin/src/pages/streams/index.tsx
@@ -4,7 +4,7 @@ import Viewer from '@/components/viewer'
 
 function StreamPage() {
   return (
-    <Box>
+    <Box display={'flex'} flex={1} minHeight={'100%'}>
       <Viewer />
     </Box>
   )
--- admin/src/pages/users/index.tsx
+++ admin/src/pages/users/index.tsx
@@ -1,28 +1,28 @@
-import { Box, Container, Table, TableCell, TableContainer, TableHead, Typography } from "@mui/material";
+import { Box, Container, Table, TableCell, TableContainer, TableHead, Typography } from '@mui/material'
 
 function UsersPage() {
-    return <Box display={"flex"} flex={1} minHeight={"100%"} >
-        <Container>
-            <TableContainer sx={{ border: 1, borderColor: 'divider', borderRadius: 2 }}>
-                <Table>
-                    <TableHead>
-                        <TableCell>
-                            <Typography fontWeight={600}>Name</Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography fontWeight={600}>Email</Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography fontWeight={600}>Last login</Typography>
-                        </TableCell>
-                        <TableCell>
-                            <Typography fontWeight={600}>Last activity</Typography>
-                        </TableCell>
-                    </TableHead>
-                </Table>
-            </TableContainer>
-        </Container>
+  return (
+    <Box display={'flex'} flex={1} minHeight={'100%'}>
+      <iframe
+        title="Speckle"
+        src="https://app.speckle.systems/projects/e8b775d472/models/da3f774e71#embed=%7B%22isEnabled%22%3Atrue%7D"
+        width="100%"
+        height="100%"
+        frameBorder={0}
+      ></iframe>
+      {/* <iframe
+        width={'100%'}
+        className="sketchfab-model"
+        data-sketchfab-model="8cbdb44f5d624d22a3b0edd6781320c3"
+        src="https://sketchfab.com/models/8cbdb44f5d624d22a3b0edd6781320c3/embed?autostart=0&transparent=1&ui_infos=0&ui_start=0&scrollwheel=1"
+        allowFullScreen
+        mozallowfullscreen="true"
+        webkitallowfullscreen="true"
+        onmousewheel=""
+        tabindex="-1"
+      ></iframe> */}
     </Box>
+  )
 }
 
-export default UsersPage;
+export default UsersPage
